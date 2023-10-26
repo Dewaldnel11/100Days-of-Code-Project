2cb329a996adb021a1a09635ec49361b9e20e0cf
Dewaldnel11 <dewaldnel.nel@gmail.com>
2023-10-26 13:45:41 +0200

    Added People & Updated Formula

--- TIP_Calc.py
+++ TIP_Calc.py
@@ -1,7 +1,8 @@
 print("Welcome to the tip calculator")
-Bill = input(print("What was the total of the Bill ?"))
+Bill = float(input("What was the total of the Bill ?"))
 #print(Bill)
-Tip_per = input(print("How much would you like to tip ? 10, 12, or 15"))
+Tip_per = int(input("How much would you like to tip ? 10, 12, or 15"))
 #print(Tip_per)
-Tip_per_person = float(Bill)/int(iTip_per)*100
+people = int(input("How many people to split the bill"))
+Tip_per_person = (Bill/Tip_per*100)/people
 print(Tip_per_person)
