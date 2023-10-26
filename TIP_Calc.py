48f6974b4e41daccd50e9060c9f3fe1d56cf23fc
Dewaldnel11 <dewaldnel.nel@gmail.com>
2023-10-26 13:38:06 +0200

    Base code complete with 1 error

---
+++ TIP_Calc.py
@@ -0,0 +1,7 @@
+print("Welcome to the tip calculator")
+Bill = input(print("What was the total of the Bill ?"))
+#print(Bill)
+Tip_per = input(print("How much would you like to tip ? 10, 12, or 15"))
+#print(Tip_per)
+Tip_per_person = float(Bill)/int(iTip_per)*100
+print(Tip_per_person)
