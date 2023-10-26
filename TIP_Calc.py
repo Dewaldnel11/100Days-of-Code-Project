7d8bd088d61760d7346d8caf927a2f081b57cf57
Dewaldnel11 <dewaldnel.nel@gmail.com>
2023-10-26 13:51:18 +0200

    Final Code rounded to 2 Decimal Points

--- TIP_Calc.py
+++ TIP_Calc.py
@@ -1,8 +1,10 @@
 print("Welcome to the tip calculator")
 Bill = float(input("What was the total of the Bill ?"))
 #print(Bill)
-Tip_per = int(input("How much would you like to tip ? 10, 12, or 15"))
+Tip = int(input("How much would you like to tip ? 10, 12, or 15"))
 #print(Tip_per)
 people = int(input("How many people to split the bill"))
-Tip_per_person = (Bill/Tip_per*100)/people
-print(Tip_per_person)
+Bill_with_tip = Tip / 100 * Bill + Bill
+Bill_per_person = Bill_with_tip / people
+round(Bill_per_person,2)
+print(Bill_per_person)
