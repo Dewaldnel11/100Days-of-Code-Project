bdcb5ccf2e5bebce552d7fa1fac0c60cad7247b0
Dewaldnel11 <dewaldnel.nel@gmail.com>
2023-10-26 13:57:12 +0200

    Updated 'round' and cleaned up code

--- TIP_Calc.py
+++ TIP_Calc.py
@@ -1,11 +1,10 @@
 print("Welcome to the tip calculator")
-Bill = float(input("What was the total of the Bill ?"))
+Bill = float(input("What was the total of the Bill ?$"))
 #print(Bill)
 Tip = int(input("How much would you like to tip ? 10, 12, or 15"))
 #print(Tip_per)
 people = int(input("How many people to split the bill"))
 Bill_with_tip = Tip / 100 * Bill + Bill
 Bill_per_person = Bill_with_tip / people
-round(Bill_per_person,2)
-final_amount=Bill_per_person
+final_amount=round(Bill_per_person,2)
 print(f"Each person should pay ${final_amount}")
