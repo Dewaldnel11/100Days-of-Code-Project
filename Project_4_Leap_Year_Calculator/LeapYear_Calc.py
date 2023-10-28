fec397522074303c41e0424a2e7c990f18a4d4bc
Dewaldnel11 <dewaldnel.nel@gmail.com>
2023-10-28 19:29:41 +0200

    Turned to notes for new optimised code

--- Project_4_Leap_Year_Calculator/LeapYear_Calc.py
+++ Project_4_Leap_Year_Calculator/LeapYear_Calc.py
@@ -1,9 +1,9 @@
-year = int(input("Please input year here ie. 1977: "))
-if year % 4 == 0:
-  print("Leap year")
-elif year % 100 >= 1:
-  print("Not leap year")
-elif year % 400 == 0:
-  print("Leap year")
-else:
-  print("What is this ?")
+#year = int(input("Please input year here ie. 1977: "))
+#if year % 4 == 0:
+ # print("Leap year")
+#elif year % 100 >= 1:
+#  print("Not leap year")
+#elif year % 400 == 0:
+#  print("Leap year")
+#else:
+ # print("What is this ?")
