05960747a33e4abca66fd17be91c4cb1f89d95cc
Dewaldnel11 <dewaldnel.nel@gmail.com>
2024-01-03 19:29:30 +0200

    Finalised

--- Project_13_Loops_Range/Loops_in_range.py
+++ Project_13_Loops_Range/Loops_in_range.py
@@ -1,3 +1,8 @@
 #For loop with range
 for number in range(1,11, 3): #Number after 2nd comma indicates amount of skips between items.
     print(number)
+
+total = 0
+for number in range(1,101):
+    total += number
+print(total)
