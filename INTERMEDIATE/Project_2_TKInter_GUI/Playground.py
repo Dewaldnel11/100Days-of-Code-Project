7263263d9f8e8d89bc1afbe949e59e8fb1f0cc88
Dewald Nel <148683680+Dewaldnel11@users.noreply.github.com>
2024-06-18 04:56:11 +0200

    day27wrapped2

--- INTERMEDIATE/Project_2_TKInter_GUI/Playground.py
+++ INTERMEDIATE/Project_2_TKInter_GUI/Playground.py
@@ -12,12 +12,12 @@ print(add(3, 5, 6, 7, 23, 33, 45, 56))
 
 def calculate(**kwargs):
     print(kwargs)
-    #for key, value in kwargs.items():
-      #  print(key)
-      #  print(value)
+    for key, value in kwargs.items():
+        print(key)
+        print(value)
 
-print(kwargs["add"])
+        print(kwargs["add"])
 
-calculate(add=3, multiply=5)
+    calculate(add=3, multiply=5)
 
 calculate()
