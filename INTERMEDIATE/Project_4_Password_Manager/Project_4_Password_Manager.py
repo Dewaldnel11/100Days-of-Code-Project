fa0379f613b7737f024364a971a0d51cc6712942
Dewald Nel <148683680+Dewaldnel11@users.noreply.github.com>
2024-10-19 23:45:40 +0200

    Buttons & Entries

--- INTERMEDIATE/Project_4_Password_Manager/Project_4_Password_Manager.py
+++ INTERMEDIATE/Project_4_Password_Manager/Project_4_Password_Manager.py
@@ -15,19 +15,25 @@ canvas.create_image(100, 100, image=logo_image)
 canvas.grid(row=0, column=1)
 
 #labels
-website_label = Label(text="Website")
+website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
-email_label = Label(text="Email")
+email_label = Label(text="Email/Username:")
 email_label.grid(row=2, column=0)
-password_label = Label(text="Password")
+password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 #Entries
 website_entries = Entry(width=35)
-website_entries.grid(row=1, column=1)
+website_entries.grid(row=1, column=1, columnspan=2)
 email_entries = Entry(width=35)
-email_entries.grid(row=2, column=1)
+email_entries.grid(row=2, column=1, columnspan=2)
 password_entries = Entry(width=21)
 password_entries.grid(row=3, column=1)
+
+#Buttons
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(row=3, column=2)
+add_button = Button(text="Add", width=36)
+add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
