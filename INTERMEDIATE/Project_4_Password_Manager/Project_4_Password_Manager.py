9c3409dbd09de392c53efd7b832290a34317eee3
Dewald Nel <148683680+Dewaldnel11@users.noreply.github.com>
2024-10-19 23:28:18 +0200

    Adding of Lables

--- INTERMEDIATE/Project_4_Password_Manager/Project_4_Password_Manager.py
+++ INTERMEDIATE/Project_4_Password_Manager/Project_4_Password_Manager.py
@@ -7,10 +7,20 @@ from tkinter import *
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
+window.config(padx=20, pady=20)
 
 canvas = Canvas(height=200, width=200)
 logo_image = PhotoImage(file="logo.png")
-canvas.create_image(image=logo_image)
+canvas.create_image(100, 100, image=logo_image)
+canvas.grid(row=0, column=1)
+
+#labels
+website_label = Label(text="Website")
+website_label.grid(row=1)
+email_label = Label(text="Email")
+email_label.grid(row=2)
+password_label = Label(text="Password")
+password_label.grid(row=3)
 
 
 
