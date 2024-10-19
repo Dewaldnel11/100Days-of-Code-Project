34bdb95442f3da527b5365f63b5e4086616a9542
Dewald Nel <148683680+Dewaldnel11@users.noreply.github.com>
2024-10-19 23:06:41 +0200

    Create File with Base window

---
+++ INTERMEDIATE/Project_4_Password_Manager/Project_4_Password_Manager.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+
+
+
+
+
+
+window.mainloop()
