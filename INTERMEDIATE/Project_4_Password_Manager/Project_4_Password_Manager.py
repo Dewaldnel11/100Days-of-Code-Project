f40b3eca4e1246d0ded7e754fbb58b98b4a102e7
Dewald Nel <148683680+Dewaldnel11@users.noreply.github.com>
2024-10-19 23:33:21 +0200

    Added "entries" section

--- INTERMEDIATE/Project_4_Password_Manager/Project_4_Password_Manager.py
+++ INTERMEDIATE/Project_4_Password_Manager/Project_4_Password_Manager.py
@@ -16,12 +16,18 @@ canvas.grid(row=0, column=1)
 
 #labels
 website_label = Label(text="Website")
-website_label.grid(row=1)
+website_label.grid(row=1, column=0)
 email_label = Label(text="Email")
-email_label.grid(row=2)
+email_label.grid(row=2, column=0)
 password_label = Label(text="Password")
-password_label.grid(row=3)
+password_label.grid(row=3, column=0)
 
-
+#Entries
+website_entries = Entry(width=35)
+website_entries.grid(row=1, column=1)
+email_entries = Entry(width=35)
+email_entries.grid(row=2, column=1)
+password_entries = Entry(width=21)
+password_entries.grid(row=3, column=1)
 
 window.mainloop()
