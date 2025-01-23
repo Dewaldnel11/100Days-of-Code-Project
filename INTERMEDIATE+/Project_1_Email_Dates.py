34ba32f1371f1bc0d870dc1921274847113da985
Dewald Nel <148683680+Dewaldnel11@users.noreply.github.com>
2025-01-23 07:07:39 +0200

    Base email setup

---
+++ INTERMEDIATE+/Project_1_Email_Dates.py
@@ -0,0 +1,12 @@
+import smtplib
+
+my_email = "allaboardprojects@gmail.com"
+password = "Dncgrams123"
+
+
+connection = smtplib.SMTP("smtp.gmail.com")
+connection.starttls()
+connection.login(user=my_email , password=password)
+connection.sendmail(from_addr=my_email, to_addrs="dewaldnel.nel@gmail.com", 
+                    msg="Subject:Hello\n\nThis is the body of my email")
+connection.close()
